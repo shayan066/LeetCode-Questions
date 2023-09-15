28cea9f01ae72e6ee8514f9f728322d2166f58fa
Shayan Azeem <106797126+shayan066@users.noreply.github.com>
2023-09-15 14:49:04 +0530

    Create Solution.java

---
+++ Easy/Valid Parentheses/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> stk= new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='(' || ch=='{' || ch=='['){
+                stk.push(ch);
+            }
+            else{
+                if(stk.empty()){
+                     return false;
+                }
+            
+            if(ch==')' && stk.peek()=='('){
+                stk.pop();
+            }else if(ch=='}' && stk.peek()=='{'){
+                stk.pop();
+            }else if(ch==']' && stk.peek()=='['){
+                stk.pop();
+            }else{
+                return false;
+            }
+        }
+        }
+        return stk.empty();
+    }
+}
